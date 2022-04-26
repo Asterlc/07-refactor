cd0c0442c817adf040f3817aee41a4c9bbe8991e
Lucas Aster <asterlc102@gmail.com>
2022-04-26 00:35:34 -0300

    found any name

--- src/routes/heroRoutes.js
+++ src/routes/heroRoutes.js
@@ -36,7 +36,8 @@ class HeroRoutes extends BaseRoute {
             handler: (request, headers) => {
                 try {
                     const { skip, limit, nome } = request.query;
-                    let query = nome ? { $regex: `.*${nome}*.` } : {}
+                    // let query = nome ? { $regex: `.*${nome}*.` } : {}
+                    let query = { nome: { $regex: `.*${nome}*.` } } 
 
                     return this.db.read(query, parseInt(skip), parseInt(limit));
                 } catch (error) {
